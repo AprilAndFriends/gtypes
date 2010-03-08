1cd19edb02a6c2d5f97176961e3ce620fe52e5ba
kspes <kresimir.spes@gmail.com>
2010-03-08 11:16:36 +0000

    * copyright info in all source files * native EOL stypes for all source files

--- include/gtypes/Spline3.h
+++ include/gtypes/Spline3.h
@@ -1,3 +1,12 @@
+/************************************************************************************\
+* This source file is part of the C++ Geometry Types Library (libgtypes)             *
+* For latest info, see http://libgtypes.sourceforge.net/                             *
+**************************************************************************************
+* Copyright (c) 2010 Kresimir Spes, Boris Mikic, Domagoj Cerjan                      *
+*                                                                                    *
+* This program is free software; you can redistribute it and/or modify it under      *
+* the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+\************************************************************************************/
 #ifndef GTYPES_SPLINE3_H
 #define GTYPES_SPLINE3_H
 
