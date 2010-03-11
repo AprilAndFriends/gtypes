03c7e3a56a1d1e7efade3c5c503d1187e1fc408d
kspes <kspes@13da1c81-a97d-4991-494a-64cc7828cfa6>
2010-03-11 10:53:33 +0000

    Linux Changes

--- trunk/unittest/main.cpp
+++ trunk/unittest/main.cpp
@@ -7,7 +7,11 @@
 * This program is free software; you can redistribute it and/or modify it under      *
 * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
 \************************************************************************************/
+#ifdef _WIN32
 #include <UnitTest++.h>
+#else
+#include <unittest++/UnitTest++.h>	
+#endif
 #include "gtypes/Vector3.h"
 #include <math.h>
 
