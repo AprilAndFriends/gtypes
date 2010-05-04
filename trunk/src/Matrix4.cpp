b8156e207d5ff4ee8ca5fbf384089b02b2c2de17
kspes <kspes@13da1c81-a97d-4991-494a-64cc7828cfa6>
2010-05-04 10:57:56 +0000

    * basic Quaternion Implementation * added some Matrix4 functions

--- trunk/src/Matrix4.cpp
+++ trunk/src/Matrix4.cpp
@@ -18,10 +18,7 @@ namespace gtypes
 
 	Matrix4::Matrix4()
 	{
-		this->mat[0] = 1.0; this->mat[4] = 0.0; this->mat[8]  = 0.0; this->mat[12] = 0.0;
-		this->mat[1] = 0.0; this->mat[5] = 1.0; this->mat[9]  = 0.0; this->mat[13] = 0.0;
-		this->mat[2] = 0.0; this->mat[6] = 0.0; this->mat[10] = 1.0; this->mat[14] = 0.0;
-		this->mat[3] = 0.0; this->mat[7] = 0.0; this->mat[11] = 0.0; this->mat[15] = 1.0;
+		this->setIdentity();
 	}
 	Matrix4::Matrix4(float  m0, float  m1, float  m2, float  m3,
 					 float  m4, float  m5, float  m6, float  m7,
@@ -34,7 +31,7 @@ namespace gtypes
 		this->mat[3] = m3; this->mat[7] = m7; this->mat[11] = m11; this->mat[15] = m15;
 	}
 
-	Matrix4::Matrix4(Matrix3& m)
+	Matrix4::Matrix4(const Matrix3& m)
 	{
 		this->mat[0] = m[0]; this->mat[4] = m[3]; this->mat[8]  = m[6]; this->mat[12] = 0.0;
 		this->mat[1] = m[1]; this->mat[5] = m[4]; this->mat[9]  = m[7]; this->mat[13] = 0.0;
@@ -42,7 +39,7 @@ namespace gtypes
 		this->mat[3] = 0.0;  this->mat[7] = 0.0;  this->mat[11] = 0.0;  this->mat[15] = 1.0;
 	}
 
-	Matrix4::Matrix4(Matrix4& m)
+	Matrix4::Matrix4(const Matrix4& m)
 	{
 		this->mat[0] = m[0]; this->mat[4] = m[4]; this->mat[8]  = m[8];  this->mat[12] = m[12];
 		this->mat[1] = m[1]; this->mat[5] = m[5]; this->mat[9]  = m[9];  this->mat[13] = m[13];
@@ -57,7 +54,33 @@ namespace gtypes
 		this->mat[2] = m[2]; this->mat[6] = m[6]; this->mat[10] = m[10]; this->mat[14] = m[14];
 		this->mat[3] = m[3]; this->mat[7] = m[7]; this->mat[11] = m[11]; this->mat[15] = m[15];
 	}
+
+	void Matrix4::set(float  m0, float  m1, float  m2, float  m3,
+		float  m4, float  m5, float  m6, float  m7,
+		float  m8, float  m9, float m10, float m11,
+		float m12, float m13, float m14, float m15)
+	{
+		*this=Matrix4(m0 ,m1 ,m2 ,m3,
+					  m4 ,m5 ,m6 ,m7,
+					  m8 ,m9 ,m10,m11,
+					  m12,m13,m14,m15);
+	}
 	
+	void Matrix4::set(float *m)
+	{
+		*this=Matrix4(m);
+	}
+	
+	void Matrix4::set(const Matrix3& m)
+	{
+		*this=Matrix4(m);
+	}
+	
+	void Matrix4::set(const Matrix4& m)
+	{
+		*this=m;
+	}
+
 	Matrix4::Matrix4(const gtypes::Vector3& axis, float angle)
 	{
 		float rad = angle*57.295779513082320876798154814105f;
@@ -88,28 +111,51 @@ namespace gtypes
 								  0, 			0, 			   0, 1);
 	}
 
-	Matrix4 Matrix4::inverse() const
+	void Matrix4::inverse()
 	{
-		Matrix4 ret;
+		float m[16];
 		float idet = 1.0f / det();
-		ret[0]  =  (this->mat[5] * this->mat[10] - this->mat[9] * this->mat[6]) * idet;
-		ret[1]  = -(this->mat[1] * this->mat[10] - this->mat[9] * this->mat[2]) * idet;
-		ret[2]  =  (this->mat[1] * this->mat[6]  - this->mat[5] * this->mat[2]) * idet;
-		ret[3]  = 0.0;
-		ret[4]  = -(this->mat[4] * this->mat[10] - this->mat[8] * this->mat[6]) * idet;
-		ret[5]  =  (this->mat[0] * this->mat[10] - this->mat[8] * this->mat[2]) * idet;
-		ret[6]  = -(this->mat[0] * this->mat[6]  - this->mat[4] * this->mat[2]) * idet;
-		ret[7]  = 0.0;
-		ret[8]  =  (this->mat[4] * this->mat[9] - this->mat[8] * this->mat[5]) * idet;
-		ret[9]  = -(this->mat[0] * this->mat[9] - this->mat[8] * this->mat[1]) * idet;
-		ret[10] =  (this->mat[0] * this->mat[5] - this->mat[4] * this->mat[1]) * idet;
-		ret[11] = 0.0;
-		ret[12] = -(this->mat[12] * (ret)[0] + this->mat[13] * (ret)[4] + this->mat[14] * (ret)[8]);
-		ret[13] = -(this->mat[12] * (ret)[1] + this->mat[13] * (ret)[5] + this->mat[14] * (ret)[9]);
-		ret[14] = -(this->mat[12] * (ret)[2] + this->mat[13] * (ret)[6] + this->mat[14] * (ret)[10]);
-		ret[15] = 1.0;
+		m[0]  =  (this->mat[5] * this->mat[10] - this->mat[9] * this->mat[6]) * idet;
+		m[1]  = -(this->mat[1] * this->mat[10] - this->mat[9] * this->mat[2]) * idet;
+		m[2]  =  (this->mat[1] * this->mat[6]  - this->mat[5] * this->mat[2]) * idet;
+		m[3]  = 0.0;
+		m[4]  = -(this->mat[4] * this->mat[10] - this->mat[8] * this->mat[6]) * idet;
+		m[5]  =  (this->mat[0] * this->mat[10] - this->mat[8] * this->mat[2]) * idet;
+		m[6]  = -(this->mat[0] * this->mat[6]  - this->mat[4] * this->mat[2]) * idet;
+		m[7]  = 0.0;
+		m[8]  =  (this->mat[4] * this->mat[9] - this->mat[8] * this->mat[5]) * idet;
+		m[9]  = -(this->mat[0] * this->mat[9] - this->mat[8] * this->mat[1]) * idet;
+		m[10] =  (this->mat[0] * this->mat[5] - this->mat[4] * this->mat[1]) * idet;
+		m[11] = 0.0;
+		m[12] = -(this->mat[12] * (m)[0] + this->mat[13] * (m)[4] + this->mat[14] * (m)[8]);
+		m[13] = -(this->mat[12] * (m)[1] + this->mat[13] * (m)[5] + this->mat[14] * (m)[9]);
+		m[14] = -(this->mat[12] * (m)[2] + this->mat[13] * (m)[6] + this->mat[14] * (m)[10]);
+		m[15] = 1.0;
 		
-		return ret;
+		set(m);
+	}
+	
+	
+	void Matrix4::inverseRotation()
+	{
+		set(this->mat[0],  this->mat[4],  this->mat[8],  this->mat[3],
+			this->mat[1],  this->mat[5],  this->mat[9],  this->mat[7],
+			this->mat[2],  this->mat[6],  this->mat[10], this->mat[11],
+			this->mat[12], this->mat[13], this->mat[14], this->mat[15]);
+	}
+	
+	Matrix4 Matrix4::inversed() const
+	{
+		Matrix4 mat(*this);
+		mat.inverse();
+		return mat;
+	}
+	
+	Matrix4 Matrix4::inversedRotation() const
+	{
+		Matrix4 mat(*this);
+		mat.inversedRotation();
+		return mat;
 	}
 	
 	void Matrix4::setZero()
@@ -218,14 +264,6 @@ namespace gtypes
 					   this->mat[1], this->mat[5], this->mat[9],  this->mat[13],
 					   this->mat[2], this->mat[6], this->mat[10], this->mat[14],
 					   this->mat[3], this->mat[7], this->mat[11], this->mat[15]);
-	}
-	
-	Matrix4 Matrix4::inverseRotation() const
-	{
-		return Matrix4(this->mat[0], this->mat[4], this->mat[8],  this->mat[3],
-					   this->mat[1], this->mat[5], this->mat[9],  this->mat[7],
-					   this->mat[2], this->mat[6], this->mat[10], this->mat[11],
-					   this->mat[12], this->mat[13], this->mat[14], this->mat[15]);
 	}
 	
 	void Matrix4::translate(const gtypes::Vector3& v)
