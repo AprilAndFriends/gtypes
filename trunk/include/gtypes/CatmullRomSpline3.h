6b6cc504a4576a9bd23143487d20350fb493fb08
boris.blizzard <boris.blizzard@13da1c81-a97d-4991-494a-64cc7828cfa6>
2014-10-30 12:40:28 +0000

    - finished docs - refactored CatmullRomSpline3 - refactored CatmullRomSpline2

--- trunk/include/gtypes/CatmullRomSpline3.h
+++ trunk/include/gtypes/CatmullRomSpline3.h
@@ -8,76 +8,149 @@
 /// 
 /// @section DESCRIPTION
 /// 
-/// DOCUMENT ME
+/// Represents a Catmull Rom Spline in 3D space.
 
 #ifndef GTYPES_CATMULL_ROM_SPLINE_3_H
 #define GTYPES_CATMULL_ROM_SPLINE_3_H
 
-#include <list>
 #include <map>
 #include <vector>
 
-#include "Vector3.h"
-
 #include "gtypesExport.h"
+#include "Vector3.h"
 
 namespace gtypes
 {
+	/// @brief Represents a Catmull Rom Spline in 3D space.
 	class gtypesExport CatmullRomSpline3
 	{
 	public:
+		/// @brief Basic Constructor.
 		CatmullRomSpline3();
-		CatmullRomSpline3(std::vector<Vector3>& vectors, bool closed = false, Vector3 t1 = Vector3(), Vector3 t2 = Vector3() );
-		CatmullRomSpline3(std::list<Vector3>& vectors, bool closed = false, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
-		CatmullRomSpline3(Vector3 vectors[], int n, bool closed = false, Vector3 t1 = Vector3(),  Vector3 t2 = Vector3());
+		/// @brief Constructor.
+		/// @param[in] vectors Points in 3D space to define the CatmullRomSpline3.
+		/// @param[in] closed Whether the CatmullRomSpline3 is closed.
+		/// @param[in] samples How many samples to use for calculation.
+		/// @param[in] t1 Custom beginning point.
+		/// @param[in] t2 Custom ending point.
+		CatmullRomSpline3(const std::vector<Vector3>& vectors, bool closed = false, int samples = 16, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
+		/// @brief Constructor.
+		/// @param[in] vectors Array of points in 3D space to define the CatmullRomSpline3.
+		/// @param[in] n Number of points in vectors.
+		/// @param[in] closed Whether the CatmullRomSpline3 is closed.
+		/// @param[in] samples How many samples to use for calculation.
+		/// @param[in] t1 Custom beginning point.
+		/// @param[in] t2 Custom ending point.
+		CatmullRomSpline3(const Vector3 vectors[], int n, bool closed = false, int samples = 16, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
+		/// @brief Destructor.
 		~CatmullRomSpline3();
 		
+		/// @brief Sets the CatmullRomSpline3's values.
+		/// @param[in] vectors Points in 3D space to define the CatmullRomSpline3.
+		/// @param[in] closed Whether the CatmullRomSpline3 is closed.
+		/// @param[in] samples How many samples to use for calculation.
+		/// @param[in] t1 Custom beginning point.
+		/// @param[in] t2 Custom ending point.
+		void set(const std::vector<Vector3>& vectors, bool closed = false, int samples = 16, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
+		/// @brief Sets the CatmullRomSpline3's values.
+		/// @param[in] vectors Array of points in 3D space to define the CatmullRomSpline3.
+		/// @param[in] n Number of points in vectors.
+		/// @param[in] closed Whether the CatmullRomSpline3 is closed.
+		/// @param[in] samples How many samples to use for calculation.
+		/// @param[in] t1 Custom beginning point.
+		/// @param[in] t2 Custom ending point.
+		void set(const Vector3 vectors[], int n, bool closed = false, int samples = 16, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
+
+		/// @return The length of the CatmullRomSpline3.
+		inline double getLength() const { return this->length; }
+		/// @return True if the CatmullRomSpline3 is closed.
+		inline bool isClosed() const { return this->closed; }
+		
+		/// @brief Calculates the position of a point on the CatmullRomSpline3.
+		/// @param[in] t Segment range position.
+		/// @return Vector3 position.
+		/// @note t is in range [0,1].
 		Vector3 calcPosition(double t);
+		/// @brief Calculates the tangent of a point on the CatmullRomSpline3.
+		/// @param[in] t Segment range position.
+		/// @return Vector3 tangent.
+		/// @note t is in range [0,1].
 		Vector3 calcTangent(double t);
+		/// @brief Calculates the static tangent of a point on the CatmullRomSpline3.
+		/// @param[in] t Segment range position.
+		/// @return Vector3 static tangent.
+		/// @note t is in range [0,1].
 		Vector3 calcStaticTangent(double t);
+		/// @brief Calculates the normal of a point on the CatmullRomSpline3.
+		/// @param[in] t Segment range position.
+		/// @return Vector3 normal.
+		/// @note t is in range [0,1].
 		Vector3 calcNormal(double t);
-		
-		Vector3 getTangent() const;
-		Vector3 getNormal() const;
-		
-		void addPoint(Vector3 point);
-		void addPoint(float x, float y, float z);
-		
-		void compile(std::vector<Vector3>& vectors, bool closed = false, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
-		void compile(std::list<Vector3>& vectors, bool closed = false, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
-		void compile(Vector3 vectors[], int n, bool closed = false, Vector3 t1 = Vector3(), Vector3 t2 = Vector3());
-		
-		void setLengthSamplingRate(int r);
-		void setCurvature(double c);
-		
-		double getLength() const;
-		
+
 	protected:
-		int _numSamples;
-		int _numSegments;
-		bool _closed;
-		double _length;
-		double _c;
-		
-		bool _inflexed;
-		Vector3 _prevNor;
-		double _prevDot;
-		
-		gvec3 prevTangent;
-		
-		std::vector<Vector3> _points;
-		std::vector<double> _lengths;
-		
-		std::map<double, int> _arcLengthMap;
-		int _prevIndex;
-		double _prevlen;
-	
+		/// @brief Whether the CatmullRomSpline3 is closed.
+		bool closed;
+		/// @brief The length.
+		double length;
+		/// @brief The curvature parameter.
+		double curvature;
+		/// @brief The number of samples used for calculating the CatmullRomSpline3.
+		int samples;
+		/// @brief The points defining the CatmullRomSpline3.
+		std::vector<Vector3> points;
+		/// @brief The lengths of all segments.
+		std::vector<double> lengths;
+
+		/// @brief Pre-calculates the length as length-calculation is expensive.
+		/// @return The length.
 		double _calcLength();
+		/// @brief Calculates the length of a segment.
+		/// @param[in] index Index of the segment.
+		/// @return The length of a segment.
 		double _calcSegmentLength(int index);
+		/// @brief Calculates the position of a segment.
+		/// @param[in] t Segment range position.
+		/// @param[in] index Index of the segment.
+		/// @return The position of a segment.
+		/// @note t is in range [0,1].
 		Vector3 _calcSegmentPosition(double t, int index);
+		/// @brief Calculates the tangent of a segment.
+		/// @param[in] t Segment range position.
+		/// @param[in] index Index of the segment.
+		/// @return The tangent of a segment.
+		/// @note t is in range [0,1].
 		Vector3 _calcSegmentTangent(double t, int index);
+		/// @brief Calculates the normal of a segment.
+		/// @param[in] t Segment range position.
+		/// @param[in] index Index of the segment.
+		/// @return The normal of a segment.
+		/// @note t is in range [0,1].
 		Vector3 _calcSegmentNormal(double t, int index);
+		/// @brief Reparametrizes the lengths of a each arc.
 		void _arcLengthReparametrization();
+
+	private:
+		/// @brief Previously stored index for length-calculation.
+		/// @note Used internally only.
+		int _prevIndex;
+		/// @brief Previously calculated length.
+		/// @note Used internally only.
+		double _prevLength;
+		/// @brief Previously calculated tangent
+		/// @note Used internally only.
+		Vector3 _prevTangent;
+		/// @brief The length of every arc.
+		/// @note Used internally only.
+		std::map<double, int> _arcLengths;
+		/// @brief Previously calculated dot-product for normal-calculation.
+		/// @note Used internally only.
+		double _prevDot;
+		/// @brief Previously calculated normal.
+		/// @note Used internally only.
+		Vector3 _prevNormal;
+		/// @brief Whether the normal is inflexed.
+		/// @note Used internally only.
+		bool _inflexed;
 
 	};
 
