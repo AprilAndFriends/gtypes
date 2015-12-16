106f36e61c0a0713a8744927fb530b9c0820127d
Boris Mikic <boris.blizzard@gmail.com>
2015-12-16 12:05:52 +0100

    - setup basic first proper unittest

--- unittest/Rectangle.cpp
+++ unittest/Rectangle.cpp
@@ -1,4 +1,4 @@
-#define __HL_TEST_LIB_NAME gtypes
+#define __HL_UT_LIB_NAME gtypes
 #ifdef __APPLE__
 #import "hunittest.h"
 #else
@@ -8,7 +8,7 @@
 #include "Rectangle.h"
 #include "Vector2.h"
 
-HL_TEST_NAMESPACE(Rectangle)
+HL_UT_TEST_CLASS(Rectangle)
 {	
 	static const float tolerance = 0.1f;
 
@@ -28,222 +28,222 @@ HL_TEST_NAMESPACE(Rectangle)
 			heqf(r1.w, r2.w, tolerance) && heqf(r1.h, r2.h, tolerance));
 	}
 
-    void assignment()
+	HL_UT_TEST_FUNCTION(assignment)
     {
 		grect r(1, 2, 3, 4);
-		assertTrue(r.x == 1 && r.y == 2 && r.w == 3 && r.h == 4, "grect::assignment - constructor assignment");
+		HL_UT_ASSERT(r.x == 1 && r.y == 2 && r.w == 3 && r.h == 4, "grect::assignment - constructor assignment");
         r.set(2, 3, 4, 5);
-		assertTrue(r.x == 2 && r.y == 3 && r.w == 4 && r.h == 5, "grect::assignment - set()");
+		HL_UT_ASSERT(r.x == 2 && r.y == 3 && r.w == 4 && r.h == 5, "grect::assignment - set()");
     }
 
-	void getPosition()
+	HL_UT_TEST_FUNCTION(getPosition)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.getPosition().x == 1 && r.getPosition().y == 2, "grect::getPosition - getPosition()");
+		HL_UT_ASSERT(r.getPosition().x == 1 && r.getPosition().y == 2, "grect::getPosition - getPosition()");
 	}
 
-	void setPosition()
+	HL_UT_TEST_FUNCTION(setPosition)
 	{
 		grect r(1, 2, 3, 4);
 		r.setPosition(5, 6);
-		assertTrue(r.getPosition().x == 5 && r.getPosition().y == 6, "grect::setPosition - setPosition()");
+		HL_UT_ASSERT(r.getPosition().x == 5 && r.getPosition().y == 6, "grect::setPosition - setPosition()");
 		r.setPosition(gvec2(7, 8));
-		assertTrue(r.getPosition().x == 7 && r.getPosition().y == 8, "grect::setPosition - setPosition()");
+		HL_UT_ASSERT(r.getPosition().x == 7 && r.getPosition().y == 8, "grect::setPosition - setPosition()");
 	}
 
-	void getSize()
+	HL_UT_TEST_FUNCTION(getSize)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.getSize().x == 3 && r.getSize().y == 4, "grect::setSize - getSize()");
+		HL_UT_ASSERT(r.getSize().x == 3 && r.getSize().y == 4, "grect::setSize - getSize()");
 	}
 
-	void setSize()
+	HL_UT_TEST_FUNCTION(setSize)
 	{
 		grect r(1, 2, 3, 4);
 		r.setSize(5, 6);
-		assertTrue(r.getSize().x == 5 && r.getSize().y == 6, "grect::setSize - setSize()");
+		HL_UT_ASSERT(r.getSize().x == 5 && r.getSize().y == 6, "grect::setSize - setSize()");
 		r.setSize(gvec2(7, 8));
-		assertTrue(r.getSize().x == 7 && r.getSize().y == 8, "grect::setSize - setSize()");
+		HL_UT_ASSERT(r.getSize().x == 7 && r.getSize().y == 8, "grect::setSize - setSize()");
 	}
 
-	void getCenter()
+	HL_UT_TEST_FUNCTION(getCenter)
 	{
 		grect r(2, 2, 2, 2);
-		assertTrue(r.getCenter().x == 3 && r.getCenter().y == 3, "grect::getCenter - getCenter()");
+		HL_UT_ASSERT(r.getCenter().x == 3 && r.getCenter().y == 3, "grect::getCenter - getCenter()");
 	}
 
-	void getAspect()
+	HL_UT_TEST_FUNCTION(getAspect)
 	{
 		grect r(0, 0, 4, 2);
-		assertTrue(r.getAspect() == 2, "grect::getAspect - getAspect()");
+		HL_UT_ASSERT(r.getAspect() == 2, "grect::getAspect - getAspect()");
 	}
 
-	void getTopLeft()
+	HL_UT_TEST_FUNCTION(getTopLeft)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.getTopLeft().x == 1 && r.getTopLeft().y == 2, "grect::getTopLeft - getTopLeft()");
+		HL_UT_ASSERT(r.getTopLeft().x == 1 && r.getTopLeft().y == 2, "grect::getTopLeft - getTopLeft()");
 	}
 
-	void getTopRight()
+	HL_UT_TEST_FUNCTION(getTopRight)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.getTopRight().x == 4 && r.getTopRight().y == 2, "grect::getTopRight - getTopRight()");
+		HL_UT_ASSERT(r.getTopRight().x == 4 && r.getTopRight().y == 2, "grect::getTopRight - getTopRight()");
 	}
 
-	void getBottomLeft()
+	HL_UT_TEST_FUNCTION(getBottomLeft)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.getBottomLeft().x == 1 && r.getBottomLeft().y == 6, "grect::getBottomLeft - getBottomLeft()");
+		HL_UT_ASSERT(r.getBottomLeft().x == 1 && r.getBottomLeft().y == 6, "grect::getBottomLeft - getBottomLeft()");
 	}
 
-	void getBottomRight()
+	HL_UT_TEST_FUNCTION(getBottomRight)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.getBottomRight().x == 4 && r.getBottomRight().y == 6, "grect::getBottomRight - getBottomRight()");
+		HL_UT_ASSERT(r.getBottomRight().x == 4 && r.getBottomRight().y == 6, "grect::getBottomRight - getBottomRight()");
 	}
 
-	void leftRightTopBottom()
+	HL_UT_TEST_FUNCTION(leftRightTopBottom)
 	{
 		grect r(1, 2, 3, 4);
-		assertTrue(r.left() == 1, "grect::leftRightTopBottom - left()");
-		assertTrue(r.right() == 4, "grect::leftRightTopBottom - right()");
-		assertTrue(r.top() == 2, "grect::leftRightTopBottom - top()");
-		assertTrue(r.bottom() == 6, "grect::leftRightTopBottom - bottom()");
+		HL_UT_ASSERT(r.left() == 1, "grect::leftRightTopBottom - left()");
+		HL_UT_ASSERT(r.right() == 4, "grect::leftRightTopBottom - right()");
+		HL_UT_ASSERT(r.top() == 2, "grect::leftRightTopBottom - top()");
+		HL_UT_ASSERT(r.bottom() == 6, "grect::leftRightTopBottom - bottom()");
 	}
 
-	void center()
+	HL_UT_TEST_FUNCTION(center)
 	{
 		grect r(0, 2, 4, 6);
-		assertTrue(heqf(r.getCenter().x, 2, tolerance), "grect::center - getCenterX()");
-		assertTrue(heqf(r.getCenter().y, 5, tolerance), "grect::center - getCenterY()");
+		HL_UT_ASSERT(heqf(r.getCenter().x, 2, tolerance), "grect::center - getCenterX()");
+		HL_UT_ASSERT(heqf(r.getCenter().y, 5, tolerance), "grect::center - getCenterY()");
 	}
 
-	void clipped()
+	HL_UT_TEST_FUNCTION(clipped)
 	{
 		grect r1(0, 0, 4, 4);
 		grect r2(1, 1, 4, 4);
 		r2 = r2.clipped(r1);
-		assertTrue(r2.x >= r1.x && r2.x + r2.w <= r1.x + r1.w &&
+		HL_UT_ASSERT(r2.x >= r1.x && r2.x + r2.w <= r1.x + r1.w &&
 			r2.y >= r1.y && r2.y + r2.h <= r1.y + r1.h, "grect::clipped - clipped()");
 	}
 
-	void clip()
+	HL_UT_TEST_FUNCTION(clip)
 	{
 		grect r1(0, 0, 4, 4);
 		grect r2(1, 1, 4, 4);
 		r2.clip(r1);
-		assertTrue(r2.x >= r1.x && r2.x + r2.w <= r1.x + r1.w &&
+		HL_UT_ASSERT(r2.x >= r1.x && r2.x + r2.w <= r1.x + r1.w &&
 			r2.y >= r1.y && r2.y + r2.h <= r1.y + r1.h, "grect::clip - clip()");
 	}
 
-	void intersection()
+	HL_UT_TEST_FUNCTION(intersection)
 	{
 		grect r1(0, 0, 4, 4);
 		grect r2(3, 3, 2, 2);
-		assertTrue(r2.intersects(r1) == true, "grect::intersection - intersects()");
+		HL_UT_ASSERT(r2.intersects(r1) == true, "grect::intersection - intersects()");
 	}
 
-	void contains()
+	HL_UT_TEST_FUNCTION(contains)
 	{
 		grect r1(0, 0, 4, 4);
 		grect r2(1, 1, 2, 2);
-		assertTrue(r1.contains(r2) == true, "grect::clip - clip()");
+		HL_UT_ASSERT(r1.contains(r2) == true, "grect::clip - clip()");
 	}
 
-	void pointInside()
+	HL_UT_TEST_FUNCTION(pointInside)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(1, 2);
-		assertTrue(r.isPointInside(v) == true, "grect::pointInside - isPointInside()");
-		assertTrue(r.isPointInside(v.x, v.y) == true, "grect::pointInside - isPointInside()");
+		HL_UT_ASSERT(r.isPointInside(v) == true, "grect::pointInside - isPointInside()");
+		HL_UT_ASSERT(r.isPointInside(v.x, v.y) == true, "grect::pointInside - isPointInside()");
 	}
 
-	void addition()
+	HL_UT_TEST_FUNCTION(addition)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(1, 2);
 		grect r1 = r + v;
-		assertTrue(r1.x == 1 && r1.y == 2, "grect::addition - operator+(vector)");
+		HL_UT_ASSERT(r1.x == 1 && r1.y == 2, "grect::addition - operator+(vector)");
 	}
 
-	void substraction()
+	HL_UT_TEST_FUNCTION(substraction)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(1, 2);
 		grect r1 = r - v;
-		assertTrue(r1.x == -1 && r1.y == -2, "grect::substraction - operator-(vector)");
+		HL_UT_ASSERT(r1.x == -1 && r1.y == -2, "grect::substraction - operator-(vector)");
 	}
 
-	void multiplication()
+	HL_UT_TEST_FUNCTION(multiplication)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(2, 4);
 		grect r1 = r * v;
-		assertTrue(r1.w == 8 && r1.h == 16, "grect::multiplication - operator*(vector)");
+		HL_UT_ASSERT(r1.w == 8 && r1.h == 16, "grect::multiplication - operator*(vector)");
 		r1 = r * 2;
-		assertTrue(r1.w == 8 && r1.h == 8, "grect::multiplication - operator*(float)");
+		HL_UT_ASSERT(r1.w == 8 && r1.h == 8, "grect::multiplication - operator*(float)");
 	}
 
-	void division()
+	HL_UT_TEST_FUNCTION(division)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(2, 4);
 		grect r1 = r / v;
-		assertTrue(r1.w == 2 && r1.h == 1, "grect::division - operator/(vector)");
+		HL_UT_ASSERT(r1.w == 2 && r1.h == 1, "grect::division - operator/(vector)");
 		r1 = r / 2;
-		assertTrue(r1.w == 2 && r1.h == 2, "grect::division - operator/(float)");
+		HL_UT_ASSERT(r1.w == 2 && r1.h == 2, "grect::division - operator/(float)");
 	}
 
-	void additionAssign()
+	HL_UT_TEST_FUNCTION(additionAssign)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(1, 2);
 		r += v;
-		assertTrue(r.x == 1 && r.y == 2, "grect::additionAssign - operator+=(vector)");
+		HL_UT_ASSERT(r.x == 1 && r.y == 2, "grect::additionAssign - operator+=(vector)");
 	}
 
-	void substractionAssign()
+	HL_UT_TEST_FUNCTION(substractionAssign)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(1, 2);
 		r -= v;
-		assertTrue(r.x == -1 && r.y == -2, "grect::substractionAssign - operator-=(vector)");
+		HL_UT_ASSERT(r.x == -1 && r.y == -2, "grect::substractionAssign - operator-=(vector)");
 	}
 
-	void multiplicationAssign()
+	HL_UT_TEST_FUNCTION(multiplicationAssign)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(2, 4);
 		r *= v;
-		assertTrue(r.w == 8 && r.h == 16, "grect::multiplicationAssign - operator*=(vector)");
+		HL_UT_ASSERT(r.w == 8 && r.h == 16, "grect::multiplicationAssign - operator*=(vector)");
 		r = grect(0, 0, 4, 4);
 		r *= 2;
-		assertTrue(r.w == 8 && r.h == 8, "grect::multiplicationAssign - operator*=(float)");
+		HL_UT_ASSERT(r.w == 8 && r.h == 8, "grect::multiplicationAssign - operator*=(float)");
 	}
 
-	void divisionAssign()
+	HL_UT_TEST_FUNCTION(divisionAssign)
 	{
 		grect r(0, 0, 4, 4);
 		gvec2 v(2, 4);
 		r /= v;
-		assertTrue(r.w == 2 && r.h == 1, "grect::divisionAssign - operator/=(vector)");
+		HL_UT_ASSERT(r.w == 2 && r.h == 1, "grect::divisionAssign - operator/=(vector)");
 		r = grect(0, 0, 4, 4);
 		r /= 2;
-		assertTrue(r.w == 2 && r.h == 2, "grect::divisionAssign - operator/=(float)");
+		HL_UT_ASSERT(r.w == 2 && r.h == 2, "grect::divisionAssign - operator/=(float)");
 	}
 	
-	void equal()
+	HL_UT_TEST_FUNCTION(equal)
 	{
 		grect r1(0, 0, 4, 4);
 		grect r2(0, 0, 4, 4);
-		assertTrue(r1 == r2, "grect::equal - operator==()");
+		HL_UT_ASSERT(r1 == r2, "grect::equal - operator==()");
 	}
 
-	void notEqual()
+	HL_UT_TEST_FUNCTION(notEqual)
 	{
 		grect r1(0, 0, 4, 4);
 		grect r2(0, 0, 4, 3);
-		assertTrue(r1 != r2, "grect::notEqual - operator!=()");
+		HL_UT_ASSERT(r1 != r2, "grect::notEqual - operator!=()");
 	}
 
 }
--- unittest/generated/_Rectangle.cpp
+++ unittest/generated/_Rectangle.cpp
@@ -1,70 +1,69 @@
-#define __HL_TEST_LIB_NAME gtypes
+#define __HL_UT_LIB_NAME gtypes
 #ifdef __APPLE__
 #import "hunittest.h"
 #else
 #include "hunittest.h"
 #endif
 
-HL_TEST_NAMESPACE(Rectangle)
+HL_UT_TEST_CLASS(Rectangle)
 {
-	void assignment();
-	void getPosition();
-	void setPosition();
-	void getSize();
-	void setSize();
-	void getCenter();
-	void getAspect();
-	void getTopLeft();
-	void getTopRight();
-	void getBottomLeft();
-	void getBottomRight();
-	void leftRightTopBottom();
-	void center();
-	void clipped();
-	void clip();
-	void intersection();
-	void contains();
-	void pointInside();
-	void addition();
-	void substraction();
-	void multiplication();
-	void division();
-	void additionAssign();
-	void substractionAssign();
-	void multiplicationAssign();
-	void divisionAssign();
-	void equal();
-	void notEqual();
+	HL_UT_TEST_FUNCTION(assignment);
+	HL_UT_TEST_FUNCTION(getPosition);
+	HL_UT_TEST_FUNCTION(setPosition);
+	HL_UT_TEST_FUNCTION(getSize);
+	HL_UT_TEST_FUNCTION(setSize);
+	HL_UT_TEST_FUNCTION(getCenter);
+	HL_UT_TEST_FUNCTION(getAspect);
+	HL_UT_TEST_FUNCTION(getTopLeft);
+	HL_UT_TEST_FUNCTION(getTopRight);
+	HL_UT_TEST_FUNCTION(getBottomLeft);
+	HL_UT_TEST_FUNCTION(getBottomRight);
+	HL_UT_TEST_FUNCTION(leftRightTopBottom);
+	HL_UT_TEST_FUNCTION(center);
+	HL_UT_TEST_FUNCTION(clipped);
+	HL_UT_TEST_FUNCTION(clip);
+	HL_UT_TEST_FUNCTION(intersection);
+	HL_UT_TEST_FUNCTION(contains);
+	HL_UT_TEST_FUNCTION(pointInside);
+	HL_UT_TEST_FUNCTION(addition);
+	HL_UT_TEST_FUNCTION(substraction);
+	HL_UT_TEST_FUNCTION(multiplication);
+	HL_UT_TEST_FUNCTION(division);
+	HL_UT_TEST_FUNCTION(additionAssign);
+	HL_UT_TEST_FUNCTION(substractionAssign);
+	HL_UT_TEST_FUNCTION(multiplicationAssign);
+	HL_UT_TEST_FUNCTION(divisionAssign);
+	HL_UT_TEST_FUNCTION(equal);
+	HL_UT_TEST_FUNCTION(notEqual);
 }
 
-HL_TEST_CLASS_BEGIN(Rectangle)
-
-HL_TEST_METHOD(Rectangle, assignment)
-HL_TEST_METHOD(Rectangle, getPosition)
-HL_TEST_METHOD(Rectangle, setPosition)
-HL_TEST_METHOD(Rectangle, getSize)
-HL_TEST_METHOD(Rectangle, setSize)
-HL_TEST_METHOD(Rectangle, getCenter)
-HL_TEST_METHOD(Rectangle, getTopLeft)
-HL_TEST_METHOD(Rectangle, getTopRight)
-HL_TEST_METHOD(Rectangle, getBottomLeft)
-HL_TEST_METHOD(Rectangle, getBottomRight)
-HL_TEST_METHOD(Rectangle, leftRightTopBottom)
-HL_TEST_METHOD(Rectangle, center)
-HL_TEST_METHOD(Rectangle, clipped)
-HL_TEST_METHOD(Rectangle, clip)
-HL_TEST_METHOD(Rectangle, intersection)
-HL_TEST_METHOD(Rectangle, contains)
-HL_TEST_METHOD(Rectangle, pointInside)
-HL_TEST_METHOD(Rectangle, addition)
-HL_TEST_METHOD(Rectangle, substraction)
-HL_TEST_METHOD(Rectangle, multiplication)
-HL_TEST_METHOD(Rectangle, division)
-HL_TEST_METHOD(Rectangle, additionAssign)
-HL_TEST_METHOD(Rectangle, substractionAssign)
-HL_TEST_METHOD(Rectangle, multiplicationAssign)
-HL_TEST_METHOD(Rectangle, divisionAssign)
-HL_TEST_METHOD(Rectangle, equal)
-HL_TEST_METHOD(Rectangle, notEqual)
-
-HL_TEST_CLASS_END
+HL_UT_RUN_CLASS_BEGIN(Rectangle)
+	HL_UT_RUN_METHOD(Rectangle, assignment)
+	HL_UT_RUN_METHOD(Rectangle, getPosition)
+	HL_UT_RUN_METHOD(Rectangle, setPosition)
+	HL_UT_RUN_METHOD(Rectangle, getSize)
+	HL_UT_RUN_METHOD(Rectangle, setSize)
+	HL_UT_RUN_METHOD(Rectangle, getCenter)
+	HL_UT_RUN_METHOD(Rectangle, getAspect)
+	HL_UT_RUN_METHOD(Rectangle, getTopLeft)
+	HL_UT_RUN_METHOD(Rectangle, getTopRight)
+	HL_UT_RUN_METHOD(Rectangle, getBottomLeft)
+	HL_UT_RUN_METHOD(Rectangle, getBottomRight)
+	HL_UT_RUN_METHOD(Rectangle, leftRightTopBottom)
+	HL_UT_RUN_METHOD(Rectangle, center)
+	HL_UT_RUN_METHOD(Rectangle, clipped)
+	HL_UT_RUN_METHOD(Rectangle, clip)
+	HL_UT_RUN_METHOD(Rectangle, intersection)
+	HL_UT_RUN_METHOD(Rectangle, contains)
+	HL_UT_RUN_METHOD(Rectangle, pointInside)
+	HL_UT_RUN_METHOD(Rectangle, addition)
+	HL_UT_RUN_METHOD(Rectangle, substraction)
+	HL_UT_RUN_METHOD(Rectangle, multiplication)
+	HL_UT_RUN_METHOD(Rectangle, division)
+	HL_UT_RUN_METHOD(Rectangle, additionAssign)
+	HL_UT_RUN_METHOD(Rectangle, substractionAssign)
+	HL_UT_RUN_METHOD(Rectangle, multiplicationAssign)
+	HL_UT_RUN_METHOD(Rectangle, divisionAssign)
+	HL_UT_RUN_METHOD(Rectangle, equal)
+	HL_UT_RUN_METHOD(Rectangle, notEqual)
+HL_UT_RUN_CLASS_END
